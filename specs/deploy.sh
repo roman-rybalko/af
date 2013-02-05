60a046ca6d5bc14d931ce269998a68025621ceb5
Roman Rybalko <devel@romanr.info>
2013-02-05 22:17:56 +0400

    nre specs

---
+++ specs/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -ex
+rm -Rvf /srv/www/roma/htdocs/romanr.info/tmp/af/*
+cp -av result-error.xml result-ok-link.xml result-ok.xml static all.html spec-all.txt spec-form.jpg spec-page.jpg spec-table.jpg /srv/www/roma/htdocs/romanr.info/tmp/af/
+cd /srv/www/roma/htdocs/romanr.info/tmp/
+zip -9r af.zip af
+mv -v af.zip af
+rsync -aHSzv --progress --del af host.romanr.info:/var/www/romanr.info/tmp/
