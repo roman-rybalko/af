e6c15868b7f27a0adebe5938cfb2a1718a0c01a3
Roman Rybalko <devel@romanr.info>
2013-07-17 00:58:27 +0400

    tests: smtp - removed basic test, removed extra calls spf->bwlists

--- tests/smtp/00-basic/done.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-. "$TESTCONF"
-
-cat system.ldif user.ldif \
-	| ldif_nl.pl \
-	| sed "s/src.hosts.advancedfiltering.net/$SRC_HOST/g;s/dst.hosts.advancedfiltering.net/$DST_HOST/g;" \
-	| ldif_dn.pl \
-	| ldapdel.sh
