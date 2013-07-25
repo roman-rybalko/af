c98bf9666dc51aceb8e354ab14101db1eb0c75c9
Roman Rybalko <devel@romanr.info>
2013-07-25 19:39:02 +0400

    deploy: geli_attach fix

--- deploy/tasks/065_geli_attach/remote.sh
+++ deploy/tasks/065_geli_attach/remote.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -ex
 . ./remote.conf
 geli attach -k *.key -p -d /dev/gpt/advancedfiltering
+fsck_ufs -p /dev/gpt/advancedfiltering.eli
+[ ! -e /dev/gpt/advancedfiltering.eli ]
+geli attach -k *.key -p -d /dev/gpt/advancedfiltering
 mount /dev/gpt/advancedfiltering.eli /usr/local/advancedfiltering
