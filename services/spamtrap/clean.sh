4b01ddd0435e8b9a825d9ebcd405f8ba9aaa873b
Roman Rybalko <devel@romanr.info>
2013-07-31 23:57:46 +0400

    spamtrap: EXPORT path fix for src, clean time upd

--- services/spamtrap/clean.sh
+++ services/spamtrap/clean.sh
@@ -4,5 +4,5 @@
 
 export HOME
 find $SPAM -type f -mtime +42 -print0 | xargs -0 rm -f
-#find $HAM -type f -mtime +42 -print0 | xargs -0 rm -f
-find $EXPORT -type f -mtime +3 -print0 | xargs -0 rm -f
+find $HAM -type f -mtime +42 -print0 | xargs -0 rm -f
+find $EXPORT -type f -mtime +42 -print0 | xargs -0 rm -f
