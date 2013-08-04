d48c0ad24013906f23ae4ee8422328f3eab199d7
Roman Rybalko <devel@romanr.info>
2013-08-04 19:40:22 +0400

    spamtrap: clean fix (verbose for rm)

--- services/spamtrap/clean.sh
+++ services/spamtrap/clean.sh
@@ -3,6 +3,6 @@
 . `dirname $0`/spamtrap.conf
 
 export HOME
-find $SPAM -type f -mtime +42 -print0 | xargs -0 rm -f
-find $HAM -type f -mtime +42 -print0 | xargs -0 rm -f
-find $EXPORT -type f -mtime +42 -print0 | xargs -0 rm -f
+find $SPAM -type f -mtime +42 -print0 | xargs -0 rm -fv
+find $HAM -type f -mtime +42 -print0 | xargs -0 rm -fv
+find $EXPORT -type f -mtime +42 -print0 | xargs -0 rm -fv
