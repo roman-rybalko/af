b2e72811c6a62906f8f16338e5ba87b4c646354f
Roman Rybalko <devel@romanr.info>
2013-08-07 19:52:49 +0400

    spamtrap: update fix (update fail workaround)

--- deploy/batch/spamtrap_db_update.sh
+++ deploy/batch/spamtrap_db_update.sh
@@ -4,7 +4,7 @@ cd ..
 
 rm -vf hosts_batch/*.ok
 rm -vf tasks_batch/*
-ln -sv ../tasks/410_spamtrap_update tasks_batch/
+ln -sv ../tasks/420_spamtrap_db_update tasks_batch/
 ./deploy.sh tasks_batch hosts_batch
 for f in hosts_batch/*.fail; do [ ! -e $f ]; done
 echo OK
--- services/spamtrap/update.sh
+++ services/spamtrap/update.sh
@@ -3,7 +3,9 @@
 . `dirname $0`/spamtrap.conf
 
 export HOME
+rm -Rf $UPDATE.new
+mkdir $UPDATE.new
+sa-update --updatedir $UPDATE.new --gpghomedir $HOME
+sa-compile --configpath=$CF --siteconfigpath=$CFSITE --updatedir=$UPDATE.new
 rm -Rf $UPDATE
-mkdir $UPDATE
-sa-update --updatedir $UPDATE --gpghomedir $HOME
-sa-compile --configpath=$CF --siteconfigpath=$CFSITE --updatedir=$UPDATE
+mv $UPDATE.new $UPDATE
