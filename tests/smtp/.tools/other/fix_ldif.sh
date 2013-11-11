5f258d5e6cc243c11434d5986d5ecc65eb858fc1
Roman Rybalko <devel@romanr.info>
2013-11-11 22:23:54 +0400

    tests: smtp fix tools

---
+++ tests/smtp/.tools/other/fix_ldif.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+while [ -n "$1" ]
+do
+	#./fix.pl < $1 > $1.new
+	#sed -r 's/afUServiceRealm=(\S+?),afUServiceName=(\S+?),ou=user,o=advancedfiltering/afUServiceRealm=\1+afUServiceName=\2,ou=user,o=advancedfiltering/i' < $1 > $1.new
+	#perl -e 'while(<>){if(/^\s*$/){$flag=1;next;}; print "\n" if $flag; $flag=0; print;}' < $1 > $1.new
+	#perl -e 'while(<>){$flag=1 if /dn: afUServiceName=\S+?,ou=user,o=advancedfiltering/i; print unless $flag; $flag=0 if /^\s*$/;}' < $1 > $1.new
+	#./.tools/ldif_nl.pl < $1 > $1.new
+	#diff -u $1 $1.new || true
+	#mv -f $1.new $1
+	shift
+done
