ced4dee3efd61c0fec184d035aee930dedb32775
Roman Rybalko <devel@romanr.info>
2013-11-26 09:46:04 +0400

    ldaprepl: refactoring

--- services/ldaprepl/ldaprepl-wd.sh
+++ services/ldaprepl/ldaprepl-wd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-L=/tmp/ldaprepl-work.lock
+L=/tmp/ldaprepl.lock
 if [ -e $L ] && kill -0 `cat $L`; then
 	exit 0
 fi
 echo $$ > $L
-`dirname $0`/work.sh 2>&1 | tail -n 1000
+`dirname $0`/ldaprepl.sh 2>&1 | tail -n 1000
 rm -f $L
---
+++ services/ldaprepl/ldaprepl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -ex
+
+. `dirname $0`/ldaprepl.conf
+exec `dirname $0`/ldaprepl.pl -U $LDAP -D $LDAPDN -W $LDAPPW -n `hostname` -s "$SYNCREPL" -v 2
