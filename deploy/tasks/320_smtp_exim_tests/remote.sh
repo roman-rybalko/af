78234df12d9fc8995b10f9511a3601bf734a63ed
Roman Rybalko <devel@romanr.info>
2014-08-29 13:29:12 +0400

    deploy: smtp_tests fix

--- deploy/tasks/320_smtp_exim_tests/remote.sh
+++ deploy/tasks/320_smtp_exim_tests/remote.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -ex
 . ./remote.conf
 
-cp -Rfv tests-smtp* /usr/local/etc/exim/ssl/
-chown -R exim /usr/local/etc/exim/ssl/tests-smtp*
-chmod -R og-rwx /usr/local/etc/exim/ssl/tests-smtp*
+cp -Rfv tests-smtp* /usr/local/advancedfiltering/smtp/ssl/
+chown -R advancedfiltering_smtp:advancedfiltering_smtp /usr/local/advancedfiltering/smtp/ssl/tests-smtp*
