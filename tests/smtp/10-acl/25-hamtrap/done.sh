ebaae3d486dce11c5d93ac25f5b60908d6110bd0
Roman Rybalko <devel@romanr.info>
2014-09-15 16:02:24 +0400

    smtp: acl tests done

---
+++ tests/smtp/10-acl/25-hamtrap/done.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -x
+
+. "$TESTCONF"
+
+add_ldif system-ht-off.ldif
--- tests/smtp/10-acl/40-data/50-incoming/30-sender/30-off/test.sh
+++ tests/smtp/10-acl/40-data/50-incoming/30-sender/30-off/test.sh
@@ -2,4 +2,4 @@
 
 . "$TESTCONF"
 
-swaks -f test2@tests.advancedfiltering.net -t mbox2@test2.com -s $DST_HOST -h-Message-ID test-message-1 | grep -E "5[[:digit:]][[:digit:]].Bad sender \\(Several senders for the same message\\) id="
+swaks -f test2@tests.advancedfiltering.net -t mbox@test.com -s $DST_HOST -h-Message-ID test-message-1 | grep -E "5[[:digit:]][[:digit:]].Bad sender \\(Several senders for the same message\\) id="
