f757d8ff0252f58ce04bb32f970d9e28f5a9481c
Roman Rybalko <devel@romanr.info>
2014-11-21 20:42:35 +0300

    tests: submission smtp auth (in progress)

--- tests/submission/10-helo/10-smtp-auth/10-client/test.sh
+++ tests/submission/10-helo/10-smtp-auth/10-client/test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -ex
 . "$TESTCONF"
+
+swaks -tls -a LOGIN -au cli1 -ap cli1password  -f test@tests.advancedfiltering.net -t mbox@test.com -s $DST_HOST -p submission -q auth
+swaks -tls -a PLAIN -au cli1 -ap cli1password  -f test@tests.advancedfiltering.net -t mbox@test.com -s $DST_HOST -p submission -q auth
--- tests/submission/10-helo/30-ip/test.sh
+++ tests/submission/10-helo/30-ip/test.sh
@@ -2,4 +2,4 @@
 
 . "$TESTCONF"
 
-swaks -f test@tests.advancedfiltering.net -t mail@test.com -s $DST_HOST -p submission -q helo | grep Hallo | grep -E 'submission-test IP'
+swaks -f test@tests.advancedfiltering.net -t mail@test.com -s $DST_HOST -p submission -q helo | grep Hallo | grep -E 'cli1 IP'
