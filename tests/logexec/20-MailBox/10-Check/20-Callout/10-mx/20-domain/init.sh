638d0ccbd9063bbb43af0ff733c9bf86bfb24309
Roman Rybalko <devel@romanr.info>
2015-06-03 02:09:27 +0300

    tests: logexec Callout test for absent mbox data

--- tests/logexec/20-MailBox/10-Check/20-Callout/10-mx/20-domain/init.sh
+++ tests/logexec/20-MailBox/10-Check/20-Callout/10-mx/20-domain/init.sh
@@ -5,5 +5,5 @@
 add_ldif user-init.ldif
 add_ldif user.ldif
 
-start_server mx -s 1 -e mx.env -p 22525
+start_server mx -s 2 -e mx.env -p 22525
 rm -f mx.env
--- tests/logexec/20-MailBox/10-Check/20-Callout/10-mx/30-client/test.sh
+++ tests/logexec/20-MailBox/10-Check/20-Callout/10-mx/30-client/test.sh
@@ -2,5 +2,7 @@
 . "$TESTCONF"
 
 "$TESTDIR"/.target/logexec-cli.sh 'AdvancedFiltering/MailBox/Check/Callout<mbox@test.com>'
+"$TESTDIR"/.target/logexec-cli.sh 'AdvancedFiltering/MailBox/Check/Callout<mbox2@test.com>'
 wait_file mx.env
 grep mbox@test.com mx.env
+grep mbox2@test.com mx.env
