78c17410861189b50a933e8dff98c8aeaf397d94
Roman Rybalko <devel@romanr.info>
2015-06-26 19:44:25 +0300

    smtp: batv for redirect - tests (ticket:7)

--- tests/smtp/20-routing/60-redirect/20-domain/test.sh
+++ tests/smtp/20-routing/60-redirect/20-domain/test.sh
@@ -4,5 +4,5 @@
 
 swaks -f test@tests.advancedfiltering.net -t mbox@test.com -s $DST_HOST -h-Message-ID test-message-id-2 -h-References test-outgoing-message-2
 wait_file submission.env
-grep MAIL submission.env | grep "domain-tech@"
+grep MAIL submission.env | grep "@batv."
 grep RCPT submission.env | grep "domain-redirect@"
--- tests/smtp/20-routing/90-submission/test.sh
+++ tests/smtp/20-routing/90-submission/test.sh
@@ -4,5 +4,4 @@
 
 swaks -f test@tests.advancedfiltering.net -t mbox@test.com -s $DST_HOST
 wait_file submission.env
-grep MAIL submission.env | grep "client-tech@"
 grep RCPT submission.env | grep "client-redirect@"
