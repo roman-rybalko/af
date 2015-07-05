094215e127d89d2b33d9058321718d9f256db7d1
Roman Rybalko <devel@romanr.info>
2015-07-05 21:25:57 +0300

    submission: shutdown (ticket:25)

--- deploy/tasks/550_submission_exim_del/remote.sh
+++ deploy/tasks/550_submission_exim_del/remote.sh
@@ -6,6 +6,12 @@
 [ -z "`pw groupshow af_submission | awk 'BEGIN{FS=":"}{print $4}'`" ]
 
 service advancedfiltering_submission_exim stop || true
+exim -C /usr/local/advancedfiltering/submission/exim/main.conf -qff || true
+exim -C /usr/local/advancedfiltering/submission/exim/shutdown.conf -qff || true
+if [ -n "`exim -C /usr/local/advancedfiltering/submission/exim/shutdown.conf -bp`" ]; then
+	service advancedfiltering_submission_exim start
+	exit 1
+fi
 rmuser -yv af_submission || true  # before rm
 rm -Rvf /usr/local/etc/rc.d/advancedfiltering_submission_exim /etc/rc.conf.d/advancedfiltering_submission_exim /usr/local/advancedfiltering/submission
 
--- services/submission/.deploy-update.sh
+++ services/submission/.deploy-update.sh
@@ -7,12 +7,13 @@ rm -vf etc/rc.d/*
 cp -vf ../advancedfiltering_submission_exim etc/rc.d/
 
 chmod -Rv u+w advancedfiltering/submission/exim
-cp -vf ../exim/configure advancedfiltering/submission/exim/
+rm -vf advancedfiltering/submission/exim/*.conf
+cp -vf ../exim/*.conf advancedfiltering/submission/exim/
 cp -vf ../../exim/scripts/mid_list_encoder.pl \
 	../../exim/scripts/dmarc_verifier.pl ../../exim/scripts/dmarc_verifier_*.dat \
 	../../exim/scripts/message_loop_detector.pl \
 	advancedfiltering/submission/exim/
-chmod a-w etc/rc.d/advancedfiltering_submission_exim advancedfiltering/submission/exim/configure \
+chmod a-w etc/rc.d/advancedfiltering_submission_exim advancedfiltering/submission/exim/*.conf \
 	advancedfiltering/submission/exim/mid_list_encoder.pl \
 	advancedfiltering/submission/exim/dmarc_verifier.pl advancedfiltering/submission/exim/dmarc_verifier_*.dat \
 	advancedfiltering/submission/exim/message_loop_detector.pl \
